f09debee26023e8ed8796e09a36fd41c08bda668
Lucas Viana <lucasvfv@gmail.com>
2023-03-28 13:39:10 -0300

    Fix/icon (#22)
    * feat: building and deploy feature added
    
    * fix: building and services markdown
    
    * feat: vitest.config for building and tests added
    
    * feat: workflow to crate release and asset added
    
    * Merge branch 'main' of github.com:jsassessoria-consultoria/monitory-software
    
    * feat: app.js is now collecting data from user machine
    
    * fix: icon is now .ico extension

--- electron/tray.ts
+++ electron/tray.ts
@@ -1,9 +1,9 @@
 import path from 'path';
-import { Menu, nativeImage, Tray } from 'electron';
+import { nativeImage, Tray } from 'electron';
 import { app } from 'electron';
 
 function createTray(TOKEN: string) {
-  const icon = path.join(app.getAppPath(), 'assets', 'icon.png'); // required.
+  const icon = path.join(app.getAppPath(), 'assets', 'icon.ico'); // required.
   const trayicon = nativeImage.createFromPath(icon);
   const tray = new Tray(trayicon.resize({ width: 16 }));
   return tray;
