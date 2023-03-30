6348d5c5d8aa325d0f60a57876b22ecc74d8bdf2
Lucas Viana <lucasvfv@gmail.com>
2023-03-30 20:41:15 -0300

    fix: sending collect data in 60 seconds now

--- src/services/callCron.ts
+++ src/services/callCron.ts
@@ -4,7 +4,7 @@ import cron from 'node-cron';
 import { cronExpression } from '../utils/cronExpressions';
 
 const callCron = () => {
-  cron.schedule(cronExpression.EVERY_10_SECONDS, collect); // Não retorna Erro
+  cron.schedule(cronExpression.EVERY_60_SECONDS, collect); // Não retorna Erro
   cron.schedule(cronExpression.EVERY_1_HOURS, readAndSend); // Não retorna Erro
 };
 
