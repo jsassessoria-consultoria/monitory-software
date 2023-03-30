e827d78d32c9abd86915b023e0590d0132fd3720
Lucas Viana <lucasvfv@gmail.com>
2023-03-30 20:43:17 -0300

    Fix/time (#26)
    * feat: building and deploy feature added
    
    * fix: building and services markdown
    
    * feat: vitest.config for building and tests added
    
    * feat: workflow to crate release and asset added
    
    * Merge branch 'main' of github.com:jsassessoria-consultoria/monitory-software
    
    * feat: app.js is now collecting data from user machine
    
    * fix: release deleting package-lock
    
    * fix: sending collect data in 60 seconds now

--- src/utils/cronExpressions.ts
+++ src/utils/cronExpressions.ts
@@ -1,5 +1,6 @@
 export const cronExpression = {
   EVERY_10_SECONDS: '*/10 * * * * *',
+  EVERY_60_SECONDS: '*/60 * * * * *',
   EVERY_15_SECONDS: '*/15 * * * * *',
   EVERY_2_HOURS: '0 */2 * * *',
   EVERY_1_HOURS: '0 */1 * * *'
